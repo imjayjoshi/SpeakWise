817eb79622d16df2964e05dd75c10abd6218c670
Jay Joshi <jayjoshi2784@gmail.com>
2025-12-01 09:30:44 +0530

    Production Ready

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,6 @@ const app = require("../backend/src/app");
 const connectDB = require("../backend/src/db/db");
 
 connectDB();
-app.listen(3000, "0.0.0.0", () => {
-  console.log("Server is running on port 3000");
+app.listen(process.env.PORT || 5000, "0.0.0.0", () => {
+  console.log(`Server is running on port ${process.env.PORT || 5000}`);
 });
--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -3,7 +3,7 @@
 import axios from "axios";
 
 const API_BASE_URL =
-  import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+  import.meta.env.VITE_API_URL;
 
 // Create axios instance
 export const api = axios.create({
